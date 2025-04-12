f210f10555e9afa4c83d39f4d21acfed8234a20d
svishal16 <shrivastavavishal640@gmail.com>
2025-04-12 18:22:08 +0530

    sonar creds params

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,4 +1,4 @@
-def call(){
+def call(string SONAR_SERVER, string SONAR_PROJECT_KEY){
     withSonarQubeEnv("${SONAR_SERVER}"){
         sh "mvn sonar:sonar -Dsonar.projectKey=${SONAR_PROJECT_KEY}"
     }
