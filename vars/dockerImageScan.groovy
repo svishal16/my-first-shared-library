e06254e89bbd4b8854a955781faad1d61766c228
svishal16 <shrivastavavishal640@gmail.com>
2025-04-12 23:59:37 +0530

    docker image scan script

---
+++ vars/dockerImageScan.groovy
@@ -0,0 +1,8 @@
+def call(String project, String imageTag, String hubUser){
+    
+    sh """
+        trivy image ${hubUser}/${project} ${hubUser}/${project}:latest > scan.txt
+        cat scan.txt
+    """
+
+}
