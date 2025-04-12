99706dd874406a129b00314df657b88bb9a109d2
svishal16 <shrivastavavishal640@gmail.com>
2025-04-12 18:20:58 +0530

    sonar creds

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,3 +1,9 @@
 def call(){
-    sh 'mvn clean package sonar:sonar'
+    withSonarQubeEnv("${SONAR_SERVER}"){
+        sh "mvn sonar:sonar -Dsonar.projectKey=${SONAR_PROJECT_KEY}"
+    }
+
+    timeout(time: 25, unit: 'MINUTES') {
+        waitForQualityGate abortPipeline: true
+    }
 }
