7c24a4c28309b9f26f6bcc2546e216c22d2898c9
svishal16 <shrivastavavishal640@gmail.com>
2025-04-13 00:19:07 +0530

    docker image push script

---
+++ vars/dockerImagePush.groovy
@@ -0,0 +1,14 @@
+def call(String project, String imageTag, String hubUser){
+
+    withCredentials([usernamePassword(
+        credentialsID: "docker-pass",
+        usernameVariable: "USER",
+        passwordVariable: "PASS"
+    )]) {
+        sh "docker login -u '$USER' -p '$PASS'"
+    }
+    
+    sh "docker image push ${hubUser}/${project}:${imageTag}"
+    sh "docker image push ${hubUser}/${project}:latest"
+
+}
