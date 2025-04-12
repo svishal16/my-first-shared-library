d50a97990a3b94a5fcc436775a0e10a85f841877
svishal16 <shrivastavavishal640@gmail.com>
2025-04-12 18:33:14 +0530

    sonar PROJ NAME

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call(){
     withSonarQubeEnv("${SONAR_SERVER}"){
-        sh "mvn sonar:sonar -Dsonar.projectKey=${SONAR_PROJECT_KEY}"
+        sh 'mvn sonar:sonar -Dsonar.projectKey=${SONAR_PROJECT_KEY} -Dsonar.projectName=${SONAR_PROJECT_NAME}'
     }
 
     timeout(time: 25, unit: 'MINUTES') {
