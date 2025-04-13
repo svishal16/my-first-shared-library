3e6985aeda753633f182014f370d470404ac8a86
svishal16 <shrivastavavishal640@gmail.com>
2025-04-13 16:51:26 +0530

    docker image clean script remove multiple

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -1,8 +1,8 @@
 def call(String project, String imageTag, String hubUser){
     
     sh """
-        docker rmi ${hubUser}/${project} ${hubUser}/${project}:${imageTag}
-        docker rmi ${hubUser}/${project} ${hubUser}/${project}:latest
+        docker rmi ${hubUser}/${project}:${imageTag}
+        docker rmi ${hubUser}/${project}:latest
     """
 
 }
